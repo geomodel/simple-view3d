2f585bacfa40b32c5988338a9f0662de06033722
alexander <alexander.thequeen@gmail.com>
2026-04-21 03:21:08 +0300

    saving

--- src/command.rs
+++ src/command.rs
@@ -1,19 +1,49 @@
 use clap::Parser;
 
+use io3d;
 use properties3d::*;
 use properties3d::types3d::*;
 
 use crate::data;
 
 const U: &str = "-999";
+const UNDEF: f32 = -999.0;
 
 //  //  //  //  //  //  //  //
+fn auto_loader(file_name: &str) -> io3d::Result<(usize, usize, usize, Box<[Option<f64>]>)> {
+    println!("autodimensions..");
+    let (h, r) = io3d::GSHeader::from_filename(file_name)?;
+    let p = io3d::GSProperty::from_gs_header(h, r)?;
+    let info: Vec<&str> = p.header.title.split_ascii_whitespace().collect();
+    if info[0] != "ijk-dims:" {
+        return Err("title is not <ijk-dims>".into());
+    }
+    let i: usize = info[1].parse()?;
+    let j: usize = info[2].parse()?;
+    let k: usize = info[3].parse()?;
+
+    let p0 = p.properties[0].clone().into_iter().map(|item| if item <= UNDEF { None } else {Some(item as f64)}).collect();
+
+    Ok((i, j, k, p0))
+}
+
+
 pub fn run() -> (data::Data3D, f32) {
     let cli = CliArgs::parse();
 
-    let grid = CachedGrid::new(cli.i_max, cli.j_max, cli.k_max);
-    let property = Property::<Continuous>::from_file(&cli.property, grid.size(), U)
-            .unwrap_or_else(|err| panic!("{}", err));
+    let (grid, property) = if let Some(i) = &cli.i_max && let Some(j) = &cli.j_max && let Some(k) = &cli.k_max {
+        let grid = CachedGrid::new(*i, *j, *k);
+        let property = Property::<Continuous>::from_file(&cli.property, grid.size(), U)
+                .unwrap_or_else(|err| panic!("Fatal: {}", err));
+        (grid, property)
+    }else{
+        let (i, j, k, data) = auto_loader(&cli.property)
+            .unwrap_or_else(|err| panic!("Fatal: {}", err));
+        let grid = CachedGrid::new(i, j, k);
+        let property = Property::<Continuous>::from_data(data);
+        (grid, property)
+    };
+
 
     let mut min_value: Option<f32> = None;
     let mut max_value: Option<f32> = None;
@@ -71,11 +101,11 @@ struct CliArgs {
     #[arg(short, long, default_value_t=1.0)]
     z_scale: f32,
     #[arg(short, long)]
-    i_max: usize,
+    i_max: Option<usize>,
     #[arg(short, long)]
-    j_max: usize,
+    j_max: Option<usize>,
     #[arg(short, long)]
-    k_max: usize,
+    k_max: Option<usize>,
     #[arg(short, long)]
     property: String,
 }
